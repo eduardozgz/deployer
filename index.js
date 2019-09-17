6206a8bff5f3c48ee93c98b278373af05e108afc
eduardozgz <eduardozgz@protonmail.com>
2019-09-17 22:24:24 +0200

    this should be correct

--- index.js
+++ index.js
@@ -4,22 +4,39 @@ const { PORT, SECRET } = process.env;
 const crypto = require("crypto");
 const express = require("express");
 const bodyParser = require("body-parser");
-const GithubWebHook = require("express-github-webhook");
 const childProcess = require("child_process");
+const bufferEq = require('buffer-equal-constant-time');
 
-const app = express();
-let webhookHandler = GithubWebHook({ path: "/deploy", secret: SECRET });
+const app = express();;
 
 app.use(bodyParser.json());
-app.use(webhookHandler);
 
-webhookHandler.on("push", (repoName, data) => {
-    const branch_name = data.ref.split("/")[data.ref.split("/").length - 1];
-    if (branch_name === "master" && repoName === "member-counter-bot") {
-        childProcess.execFile("./deploy.sh");
+app.post("/deploy", (req, res) => {
+    console.log("Webhook received");
+
+    const { body } = req;
+    const unparsedBody = JSON.stringify(req.body);
+    
+    const verified = verifySignature(SECRET, unparsedBody, req.headers['X-Hub-Signature']);
+
+    if (verified) {
+        const branchName = body.ref.split("/")[body.ref.split("/").length - 1];
+        if (branchName === "master" && body.repository.name === "deployer") {
+            console.log("Deploy requested, attemping to run script...")
+            childProcess.execFile("./deploy.sh"); //todo error logging
+        }
     }
 });
 
 app.listen(PORT, () => {
     console.log("Port:", PORT);
 });
+
+//https://github.com/Gisonrg/express-github-webhook/blob/master/index.js
+function signData(secret, data) {
+	return 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex');
+}
+
+function verifySignature(secret, data, signature) {
+	return bufferEq(new Buffer(signature), new Buffer(signData(secret, data)));
+}
