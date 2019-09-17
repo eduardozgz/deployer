8de335f0e4d246910fd829ea82fae89a9b6e57dd
eduardozgz <eduardozgz@protonmail.com>
2019-09-17 13:10:44 +0200

    tesst

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const { PORT, SECRET } = process.env;
+const crypto = require('crypto');
 const express = require("express");
 const bodyParser = require('body-parser');
+const GithubWebHook = require('express-github-webhook');
 const app = express();
+let webhookHandler = GithubWebHook({ path: '/deploy', secret: SECRET });
 
 app.use(bodyParser.json());
+app.use(webhookHandler);
 
-app.post("/deploy", (req, res) => {
-    console.log(req.header('X-Hub-Signature'))
-    
-
+webhookHandler.on('event', (repo, data) => {
+    console.log(repo, data)
 });
 
 app.listen(PORT, () => { console.log("Port:", PORT) });
