8bba3a43b208bf60ca2ece2c1fefa5f655d64808
eduardozgz <eduardozgz@protonmail.com>
2019-09-17 13:24:39 +0200

    tesst

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const { PORT, SECRET } = process.env;
-const crypto = require('crypto');
+const crypto = require("crypto");
 const express = require("express");
-const bodyParser = require('body-parser');
-const GithubWebHook = require('express-github-webhook');
+const bodyParser = require("body-parser");
+const GithubWebHook = require("express-github-webhook");
+const childProcess = require("child_process");
+
 const app = express();
-let webhookHandler = GithubWebHook({ path: '/deploy', secret: SECRET });
+let webhookHandler = GithubWebHook({ path: "/deploy", secret: SECRET });
 
 app.use(bodyParser.json());
 app.use(webhookHandler);
 
-webhookHandler.on('push', (repo, data) => {
-    console.log(repo);
-
-
+webhookHandler.on("push", (repoName, data) => {
+    console.log(data)
 });
 
-app.listen(PORT, () => { console.log("Port:", PORT) });
+app.listen(PORT, () => {
+    console.log("Port:", PORT);
+});
