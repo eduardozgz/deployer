f43f74a5ffdb4022f2aa47a981a79c5e538bdcb5
eduardozgz <eduardozgz@protonmail.com>
2019-09-17 22:38:25 +0200

    done I guess

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 #!/usr/bin/env node
 require("dotenv").config();
-const { PORT, SECRET } = process.env;
+const { PORT, SECRET, REPO_NAME, BRANCH } = process.env;
 const crypto = require("crypto");
 const express = require("express");
 const bodyParser = require("body-parser");
 const childProcess = require("child_process");
-const bufferEq = require('buffer-equal-constant-time');
 
-const app = express();;
+const app = express();
 
 app.use(bodyParser.json());
 
 app.post("/deploy", (req, res) => {
     console.log("Webhook received");
 
-    if (req.get('x-hub-signature') === signData(SECRET, JSON.stringify(req.body))) {
+    if (req.get("x-hub-signature") === signData(SECRET, JSON.stringify(req.body))) {
         console.log("Authorized ✔");
 
         const branchName = req.body.ref.split("/")[req.body.ref.split("/").length - 1];
 
-        console.log(branchName, req.body.repository.name )
-
-        if (branchName === "master" && req.body.repository.name === "deployer") {
-            console.log("Deploy requested, attemping to run script...")
-            childProcess.execFile("./deploy.sh"); //todo error logging
+        if (branchName === BRANCH && req.body.repository.name === REPO_NAME) {
+            console.log("Deploy requested, attemping to run script...");
+            childProcess.execFile("./deploy.sh", (error, stdout, stderr) => {
+                if (error) throw error;
+                console.log(stdout);
+              });
         }
     } else {
         console.log("Unauthorized ❌");
@@ -36,5 +36,11 @@ app.listen(PORT, () => {
 
 //https://github.com/Gisonrg/express-github-webhook/blob/master/index.js
 function signData(secret, data) {
-	return 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex');
+    return (
+        "sha1=" +
+        crypto
+            .createHmac("sha1", secret)
+            .update(data)
+            .digest("hex")
+    );
 }
