b0cf93386e34e59c12a1d6ad69f929a1f1b1a9b2
eduardozgz <eduardozgz@protonmail.com>
2019-09-18 22:07:58 +0200

    release I guess

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 const { deploys, port } = require("./deploys.json");
 const express = require("express");
 const bodyParser = require("body-parser");
-const runTasks = require("./runTasks");
 const signData = require("./signData");
+const fs = require("fs");
+const childProccess = require("child_process");
+
+if (!fs.existsSync("./scripts")) fs.mkdirSync("./scripts");
 
 const app = express();
 
 app.use(bodyParser.json());
-
-runTasks(deploys[0].deploy_tasks);
 
 app.post("/deploy", (req, res) => {
     console.log(`Webhook received.`);
@@ -19,15 +20,17 @@ app.post("/deploy", (req, res) => {
             repository_full_name,
             branch_to_deploy,
             secret,
-            deploy_tasks
+            deploy_scripts
         } = deploy;
         const repositoryNameMatches = repository_full_name === req.body.repository.full_name;
         const hasValidSignature = req.get("x-hub-signature") === signData(secret, JSON.stringify(req.body));
         const branchPushedIsDeployable = branch_to_deploy === req.body.ref.split("/")[req.body.ref.split("/").length - 1];
         if (repositoryNameMatches && hasValidSignature && branchPushedIsDeployable) {
             console.log(`Webhook authorized ✔`);
-            console.log(`Running deploy tasks...`);
-            runTasks(deploy_tasks);
+            console.log(`Running deploy scripts...`);
+            for (script of deploy_scripts) {
+                childProccess.execFileSync(script);
+            }
         }
     }
 });
