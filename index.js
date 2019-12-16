6e4fe7705282a11d1d13e131eca5b0e6cf5e4e44
eduardozgz <eduardozgz@protonmail.com>
2019-12-16 14:17:30 +0100

    dependency-free

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 #!/usr/bin/env node
 
 const { deploys, port } = require("./deploys.json");
-const express = require("express");
-const bodyParser = require("body-parser");
+const http = require("http");
 const signData = require("./signData");
 const fs = require("fs");
 const childProccess = require("child_process");
 
 if (!fs.existsSync("./scripts")) fs.mkdirSync("./scripts");
 
-const app = express();
+http
+    .createServer((req, res) => {
+        if (req.headers["content-type"] === "application/json") {
+            let body = "";
+            req.on("data", data => body += data);
+            req.on("end", async () => {
+                body = JSON.parse(body);
+                console.log(`Webhook received.`);
+                for (deploy of deploys) {
+                    const {
+                        repository_full_name,
+                        branch_to_deploy,
+                        secret,
+                        deploy_scripts
+                    } = deploy;
 
-app.use(bodyParser.json());
-
-app.post("/", (req, res) => {
-    console.log(`Webhook received.`);
-    for (deploy of deploys) {
-        const {
-            repository_full_name,
-            branch_to_deploy,
-            secret,
-            deploy_scripts
-        } = deploy;
-        const repositoryNameMatches = repository_full_name === req.body.repository.full_name;
-        const hasValidSignature = req.get("x-hub-signature") === signData(secret, JSON.stringify(req.body));
-        const branchPushedIsDeployable = branch_to_deploy === req.body.ref.split("/")[req.body.ref.split("/").length - 1];
-        if (repositoryNameMatches && hasValidSignature && branchPushedIsDeployable) {
-            console.log(`Webhook authorized ✔`);
-            console.log(`Running deploy scripts...`);
-            for (script of deploy_scripts) {
-                childProccess.execFileSync(script);
-            }
+                    const repositoryNameMatches = repository_full_name === req.body.repository.full_name;
+                    const hasValidSignature = req.get("x-hub-signature") === signData(secret, JSON.stringify(req.body));
+                    const branchPushedIsDeployable = branch_to_deploy === req.body.ref.split("/")[req.body.ref.split("/").length - 1];
+                    
+                    if (repositoryNameMatches && hasValidSignature && branchPushedIsDeployable) {
+                        console.log(`Webhook authorized ✔`);
+                        console.log(`Running deploy scripts...`);
+                        for (script of deploy_scripts) {
+                            childProccess.execFileSync(script);
+                        }
+                    }
+                }
+                res.statusCode = 204;
+                res.end();
+            });
+        } else {
+            res.statusCode = 415;
+            res.end();
         }
-    }
-});
-
-app.listen(port, () => {
+    })
+    .listen(port, () => {
     console.log("HTTP SERVER LISTENING @", port);
 });
